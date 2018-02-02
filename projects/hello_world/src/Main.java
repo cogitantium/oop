065c440142d2851057d0d99bb4d622bac5a44cbb
cogitantium <falke.cs@gmail.com>
2018-02-02 11:32:27 +0100

    initial commit of hello_world-project

---
+++ projects/hello_world/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello World!");
+        System.out.println("This is the equivalent command to console.log in javascript.");
+        sec();
+    }
+
+
+    public static void sec() {
+        System.out.println("This is weird.");
+    }
+}
