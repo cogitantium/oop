93648b526f362129a1d258c2ea1f002b466e51b9
cogitantium <falke.cs@gmail.com>
2018-02-13 23:45:05 +0100

    debugging JUnit installation and writing tests for assign01

--- assignments/assign01/src/player.java
+++ assignments/assign01/src/player.java
@@ -1,3 +1,7 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class player {
     String firstName;
     String lastName;
@@ -14,6 +18,12 @@ public class player {
         this.matchWins = matchWins;
         this.matchLoss = matchLoss;
         this.winLossRatio = calcRatio();
+    }
+
+    @Test
+    void testCalcRatio() {
+        player testPlayer01 = new player("Name", "Name", 22,4,3);
+        assertEquals(testPlayer01.calcRatio(), (4 / 3));
     }
 
     double calcRatio() {
