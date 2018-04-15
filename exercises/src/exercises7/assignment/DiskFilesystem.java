3277352ef38bc8bb40a35be05cbbd24618a9c7d7
cogitantium <falke.cs@gmail.com>
2018-04-15 16:17:47 +0200

    added intial work on assign7

---
+++ exercises/src/exercises7/assignment/DiskFilesystem.java
@@ -0,0 +1,27 @@
+package exercises7.assignment;
+
+/*
+Write a command line program that takes a directory as an argument.
+The program should check that the directory exists and is indeed a directory.  It should
+then compute the size of all regular files in the directory itself and its sub-directories,
+recursively.
+Hint:  You may want to use Files.size, but remember only to use it on regular files.
+You must traverse directories manually.
+
+Get inspiration from df-utility from *nix
+ */
+
+import java.nio.file.*;
+
+public class DiskFilesystem {
+
+    public static void main(String[] args) {
+        System.out.println("Stuff");
+
+    }
+
+    public double add(double comp1, double comp2) {
+        return comp1 + comp2;
+    }
+
+}
